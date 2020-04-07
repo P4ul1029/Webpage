a42d6c40394719240e2073a27cb9d99c61e8a720
Paul Barasa <pauljb64@gmail.com>
2020-04-07 13:14:03 -0400

    Filters pop up is working

--- Results_Page/script.js
+++ Results_Page/script.js
@@ -1,6 +1,34 @@
 const check = document.getElementById('check')
 const currencies = document.getElementById('Currencies')
 const search = document.getElementById('search')
+const openModalBtn = document.querySelectorAll('[data-modal-target]')
+const closeModalBtn = document.querySelectorAll('[data-close-btn')
+
+openModalBtn.forEach(label => {
+    label.addEventListener('click', () =>{
+        const modal = document.querySelector(label.dataset.modalTarget)
+        openModal(modal)
+    })
+})
+
+closeModalBtn.forEach(button => {
+    button.addEventListener('click', () =>{
+        const modal = button.closest('.filter')
+        closeModal(modal)
+    })
+})
+
+function openModal(modal) {
+    if (modal == null) return
+
+    modal.classList.add('active')
+}
+
+function closeModal(modal) {
+    if (modal == null) return
+
+    modal.classList.remove('active')
+}
 
 function toggling(id) {
     var e = document.getElementById(id);
@@ -17,6 +45,6 @@ function toggling(id) {
     }
  }
 
- function toggleCheckbox() {
+ function hideCurrencies() {
     currencies.style.display = 'none'
 }
