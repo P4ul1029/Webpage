ac5e8f586b05d9a3dc4953c880e4aeed4b485f8c
Paul Barasa <pauljb64@gmail.com>
2020-04-07 13:21:20 -0400

    Added input validation to filters

--- Results_Page/script.js
+++ Results_Page/script.js
@@ -3,6 +3,10 @@ const currencies = document.getElementById('Currencies')
 const search = document.getElementById('search')
 const openModalBtn = document.querySelectorAll('[data-modal-target]')
 const closeModalBtn = document.querySelectorAll('[data-close-btn')
+const checkIn = document.getElementById('checkIn')
+const checkOut = document.getElementById('checkOut')
+const form = document.getElementById('form')
+const errorElem = document.getElementById('error')
 
 openModalBtn.forEach(label => {
     label.addEventListener('click', () =>{
@@ -48,3 +52,34 @@ function toggling(id) {
  function hideCurrencies() {
     currencies.style.display = 'none'
 }
+
+// Date validation
+var currentDate = new Date()
+var dd = currentDate.getDate()
+var mm = currentDate.getMonth() + 1
+var yyyy = currentDate.getFullYear()
+
+if (dd < 10){
+    dd ='0' + dd
+}
+
+if (mm < 10){
+    mm='0' + mm
+}
+
+currentDate = yyyy + '-' + mm + '-' + dd
+checkIn.setAttribute("min", currentDate)
+checkOut.setAttribute("min", currentDate)
+
+form.addEventListener('submit', (e) => {
+    let errorMessages = []
+
+    if (checkIn.value === checkOut.value){
+        errorMessages.push('You can not check in and out on the same date!')
+    }
+
+    if (errorMessages.length > 0){
+        e.preventDefault()
+        errorElem.innerText = errorMessages.join("\r\n")
+    }
+})
